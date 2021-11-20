7acbc148798b61191e8622b5644f53570fd04427
rishabhbhatia57 <[rishabhbhatia57@gmail.com]>
2021-11-21 01:39:58 +0530

    Registration

--- src/Registration/Registration.js
+++ src/Registration/Registration.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import IframeResizer from "iframe-resizer-react";
+import "./Registration.css";
 class Registration extends Component {
   render() {
     return (
-        <div>
-          <h2>Registration</h2>
+      <div className="container registrationContainer">
+        <div className="row registrationRow">
+          <div className="col col-lg PanelHeading">
+            A thousand generations live in you now. But this is your fight.
+            <span className="gradient"></span>
+            <span className="dodge"></span>
+          </div>
+          <div className="col-lg">
+            <IframeResizer
+              log
+              src="https://docs.google.com/forms/d/e/1FAIpQLSeWuOurNvfhIn3c6i59xzFaevwVjGMMzoOYtdrcoVC7GE0y0g/viewform?embedded=true"
+              style={{
+                minWidth: "100%",
+                height: "500px",
+                frameborder: "0",
+                marginheight: "0",
+                marginwidth: "0",
+              }}
+              scrolling={"yes"}
+            />
+          </div>
         </div>
+      </div>
     );
   }
 }
