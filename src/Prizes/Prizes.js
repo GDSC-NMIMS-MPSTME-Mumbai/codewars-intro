d32c8e8d09caf1caea0d8b7d185f04933699cf52
AngadSinghKataria <73480824+AngadSinghKataria@users.noreply.github.com>
2021-11-20 20:32:31 +0530

    New additions

--- src/Prizes/Prizes.js
+++ src/Prizes/Prizes.js
@@ -14,8 +14,11 @@ function Prizes() {
   return (
     <div className="container DeathStar-container">
       <div className="row">
+        <div className="col col-lg DeathStar-cap">
+          May the force be with you.
+        </div>
         <div className="col col-lg DeathStar-col">
-          <img src={TrophyImg} alt="DeathStar" className="DeathStar"/>
+          <img src={TrophyImg} alt="DeathStar" className="DeathStar" />
         </div>
         <div className="col col-lg prize-col">
           <ul className="card-ul">
@@ -23,18 +26,21 @@ function Prizes() {
               <PrizeCard bgimg="1" />
             </li>
             <li className="pic2 card-li">
-              <PrizeCard bgimg="2"/>
+              <PrizeCard bgimg="2" />
             </li>
             <li className="pic3 card-li">
-              <PrizeCard bgimg="3"/>
+              <PrizeCard bgimg="3" />
             </li>
             <li className="pic4 card-li">
-              <PrizeCard bgimg="4"/>
+              <PrizeCard bgimg="4" />
             </li>
             <li className="pic5 card-li">
-              <PrizeCard bgimg="5"/>
+              <PrizeCard bgimg="5" />
             </li>
           </ul>
+        </div>
+        <div className="col col-lg DeathStar-cap2">
+          May the force be with you.
         </div>
       </div>
     </div>
