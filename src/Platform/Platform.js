c8cc3c277aaa48d023297d3f8e34e4231a82cf12
AngadSinghKataria <73480824+AngadSinghKataria@users.noreply.github.com>
2021-11-22 04:44:39 +0530

    Changes

--- src/Platform/Platform.js
+++ src/Platform/Platform.js
@@ -6,7 +6,7 @@ export default function Platform() {
   return (
     <div className="container PlatformContainer">
       <div className="col col-lg PlatformCol2">
-        A thousand generations live in you now. But this is your fight.
+        I like firsts. Good or bad, they’re always memorable
       </div>
       <div className="row">
         <div className="col col-lg PlatformCol">
